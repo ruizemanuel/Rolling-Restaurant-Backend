8fd12694cde6b374f75e150001cebde6e59ad9d4
Emanuel Ruiz <ruiz.e.642@gmail.com>
2023-04-03 19:54:05 -0300

    se agrego descripcion al producto

--- src/middlewares/productValidation.js
+++ src/middlewares/productValidation.js
@@ -17,6 +17,11 @@ const productValidate = [
         throw new Error("El precio del producto debe estar entre 0 y 10000");
       }
     }),
+  check("description")
+    .notEmpty()
+    .withMessage("La descripcion es obligatoria")
+    .isLength({ min: 2, max: 200 })
+    .withMessage("La descripcion del producto debe tener entre 2 y 200 caracteres"),
   check("urlImg")
     .notEmpty()
     .withMessage("La imagen del producto es obligatoria"),
